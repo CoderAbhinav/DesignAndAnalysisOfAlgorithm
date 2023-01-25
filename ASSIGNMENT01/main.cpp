0b1c1920068bfc49b0583326f1e7224cf67b91f3
Abhinav Belhekar <73335766+CoderAbhinav@users.noreply.github.com>
2023-01-25 14:27:44 +0530

    initial commit

---
+++ ASSIGNMENT01/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    for (int i = 0; i < argc; i++) cout << argv[i] << '\n';
+
+    return 0;
+}
